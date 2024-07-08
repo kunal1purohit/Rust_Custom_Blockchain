735426c63c9e83432a0f5d93a564f3fe7930eeb2
kunal1purohit <115582602+kunal1purohit@users.noreply.github.com>
2024-07-08 23:05:21 +0530

    test done

--- src/block.rs
+++ src/block.rs
@@ -100,6 +100,16 @@ impl Blockchain{
 }
 
 #[cfg(test)]
+
 mod tests{
     use super::*;
+
+    #[test]
+    fn test_blockchain() {
+        let mut b = Blockchain::new();
+        b.add_block("data".to_string());
+        b.add_block("data2".to_string());
+        b.add_block("data3".to_string());
+        dbg!(b);
+    }
 }
